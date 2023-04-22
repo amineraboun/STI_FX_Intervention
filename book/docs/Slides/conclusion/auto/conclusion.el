c10fa766808b11d59e43e7bc0e214fae89b5cc42
Romain Lafarguette <romain.lafarguette@adia.ae>
2023-04-22 14:47:56 +0500

    Conclusion slides

--- book/docs/Slides/conclusion/auto/conclusion.el
+++ book/docs/Slides/conclusion/auto/conclusion.el
@@ -3,6 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("caption" "font=scriptsize" "labelfont=scriptsize" "labelfont={color=imfblue}")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "beamer"
@@ -32,6 +40,8 @@
     "wideitemize"
     "wideenumerate"
     "extrawideitemize"
-    "extrawideenumerate"))
+    "extrawideenumerate")
+   (LaTeX-add-xcolor-definecolors
+    "imfblue"))
  :latex)
 
