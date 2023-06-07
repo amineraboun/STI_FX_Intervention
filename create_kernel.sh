37e3c1c1ff092b6a2f7c1bbb2b0c11e8c260bd74
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-07 08:23:54 -0700

    create kernel

--- create_kernel.sh
+++ create_kernel.sh
@@ -3,8 +3,20 @@
 name="sti"
 
 python -m venv .venv
-#source .venv/bin/activate
+
+# source .venv/bin/activate
+
+# upgrade pip
 .venv/bin/python -m pip install --upgrade pip
+
+# install all requirements
 .venv/bin/python -m pip install -r requirements.txt
+
+# install ipykernel (if not already there because of requirements)
 .venv/bin/python -m pip install ipykernel
-.venv/bin/python -m ipykernel install --user --name=$name
+
+# install a kernel in the standard location for virtual environments
+.venv/bin/python -m ipykernel install --prefix .venv --name=$name
+
+#.venv/bin/python -m ipykernel install --user --name=$name
+
