dcd5247d67e3a86a949e398d592499daac58e3a6
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-07 08:43:38 -0700

    The big clean (#3)
    * remove .idea
    
    * move to requirements.txt
    
    * remove checkpoints
    
    * _build folder
    
    * Update book.yml
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * helpers to fire up jupyter locally
    
    * create kernel

--- create_kernel.sh
+++ create_kernel.sh
@@ -2,6 +2,21 @@
 
 name="sti"
 
-poetry install
-poetry run pip install ipykernel
-poetry run python -m ipykernel install --user --name=$name
+python -m venv .venv
+
+# source .venv/bin/activate
+
+# upgrade pip
+.venv/bin/python -m pip install --upgrade pip
+
+# install all requirements
+.venv/bin/python -m pip install -r requirements.txt
+
+# install ipykernel (if not already there because of requirements)
+.venv/bin/python -m pip install ipykernel
+
+# install a kernel in the standard location for virtual environments
+.venv/bin/python -m ipykernel install --prefix .venv --name=$name
+
+#.venv/bin/python -m ipykernel install --user --name=$name
+
---
+++ jlab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+.venv/bin/jupyter lab 
+#--notebook-dir=book/docs
+
